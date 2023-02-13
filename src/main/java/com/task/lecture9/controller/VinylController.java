f9475dab9661e9740a8ac3cc60582021484f915c
sow0731 <sow.ishihara@gmail.com>
2023-02-13 14:43:11 +0900

    Patch機能追加の為のコミット

--- src/main/java/com/task/lecture9/controller/VinylController.java
+++ src/main/java/com/task/lecture9/controller/VinylController.java
@@ -2,6 +2,7 @@ package com.task.lecture9.controller;
 
 import com.task.lecture9.dto.VinylDto;
 import com.task.lecture9.form.InsertForm;
+import com.task.lecture9.form.UpdateForm;
 import com.task.lecture9.repository.entity.Vinyl;
 import com.task.lecture9.service.VinylService;
 import com.task.lecture9.service.VinylServiceImpl;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,18 +31,18 @@ public class VinylController {
         this.vinylService = vinylServiceImpl;
     }
 
-    @GetMapping("/vinyls")
+    @GetMapping("/vinyls" )
     public List<Vinyl> getVinyls() {
 
         return vinylService.findAll();
     }
 
-    @GetMapping("/vinyls/{id}")
+    @GetMapping("/vinyls/{id}" )
     public Vinyl getVinylById(@PathVariable(required = false) Integer id) throws Exception {
         return vinylService.findById(id);
     }
 
-    @PostMapping("/vinyls")
+    @PostMapping("/vinyls" )
     public ResponseEntity<Map<String, String>> create(
             @Validated @RequestBody InsertForm insertForm,
             HttpServletRequest request) {
@@ -54,6 +56,23 @@ public class VinylController {
         int id = vinylService.insert(vinylDto);
 
         URI uri = ServletUriComponentsBuilder.fromContextPath(request).path("/vinyls/" + id).build().toUri();
-        return ResponseEntity.created(uri).body(Map.of("message", "New Vinyl Data Is Added"));
+        return ResponseEntity.created(uri).body(Map.of("message", "New Vinyl Data Is Added" ));
+    }
+
+    @PatchMapping("/vinyls/{id}" )
+    public ResponseEntity<Vinyl> update(
+            @PathVariable("id" ) Integer id,
+            @Validated @RequestBody UpdateForm updateForm) throws Exception {
+
+
+        VinylDto vinylDto = new VinylDto(
+                updateForm.getTitle(),
+                updateForm.getArtist(),
+                updateForm.getLabel(),
+                updateForm.getReleaseYear());
+
+        var vinyl = vinylService.update(id, vinylDto);
+
+        return ResponseEntity.ok(vinyl);
     }
 }
--- src/main/java/com/task/lecture9/repository/mapper/VinylMapper.java
+++ src/main/java/com/task/lecture9/repository/mapper/VinylMapper.java
@@ -6,20 +6,34 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface VinylMapper {
-    @Select("SELECT * FROM vinyl")
+    @Select("SELECT * FROM vinyl" )
     List<Vinyl> findAll();
 
-    @Select("SELECT * FROM vinyl WHERE id=#{id}")
+    @Select("SELECT * FROM vinyl WHERE id=#{id}" )
     Optional<Vinyl> findById(Integer id);
 
     @Insert("INSERT INTO vinyl (id, title, artist, label, release_year) VALUES (#{id}, #{title}, #{artist}, #{label}," +
-            " #{releaseYear})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
+            " #{releaseYear})" )
+    @Options(useGeneratedKeys = true, keyProperty = "id" )
     void insert(VinylDto vinylDto);
+
+    @Update("UPDATE vinyl " +
+            "SET title=" +
+            "   CASE WHEN #{vinylDto.title} IS NOT NULL THEN #{vinylDto.title} ELSE title END," +
+            "artist=" +
+            "   CASE WHEN #{vinylDto.artist} IS NOT NULL THEN #{vinylDto.artist} ELSE artist END," +
+            "label=" +
+            "   CASE WHEN #{vinylDto.label} IS NOT NULL THEN #{vinylDto.label} ELSE label END," +
+            "release_year=" +
+            "   CASE WHEN #{vinylDto.releaseYear} IS NOT NULL THEN #{vinylDto.releaseYear} ELSE release_year END " +
+            "WHERE id=#{id}"
+    )
+    void update(Integer id, VinylDto vinylDto);
 }
